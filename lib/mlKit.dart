05103be04d4c304c27ea74af6a0354be29065c68
rocknegi <rocknegi53@gmail.com>
2018-11-13 20:57:29 +0530

    configure firebase

--- lib/mlKit.dart
+++ lib/mlKit.dart
@@ -1,15 +1,26 @@
-import './main.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+ import 'package:mlkit/mlkit.dart';
 
-class Mlkit extends StatelessWidget{
+class Mlkit extends StatelessWidget {
   final File img;
   Mlkit(this.img);
   @override
   Widget build(BuildContext context) {
-      // TODO: implement build
-      return MaterialApp(
-        home: Image.file(img),
-      );
-    }
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Lens It'),
+      ),
+      body: Container(
+        child: Container(
+          width: double.infinity,
+          height: 400,
+          color: Colors.black,
+          child: Image.file(img),
+        ),
+      ),
+    ));
+  }
 }
