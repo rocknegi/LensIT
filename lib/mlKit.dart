8d8a5366932f138838821327b0fe42d163c8c4e4
rocknegi <rocknegi53@gmail.com>
2018-11-13 22:54:35 +0530

    add image labelling

--- lib/mlKit.dart
+++ lib/mlKit.dart
@@ -1,26 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'dart:io';
- import 'package:mlkit/mlkit.dart';
+import 'package:mlkit/mlkit.dart';
+import 'dart:async';
 
-class Mlkit extends StatelessWidget {
-  final File img;
-  Mlkit(this.img);
+// void main() => runApp(new Mlkit(_file));
+
+class Mlkit extends StatefulWidget {
+   final File img;
+   Mlkit(this.img);
+ //const Mlkit({Key key, this.img}): super(key: key);
+
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<Mlkit> {
+  File _file;
+
+  List<VisionLabel> _currentLabels = <VisionLabel>[];
+
+  FirebaseVisionLabelDetector detector = FirebaseVisionLabelDetector.instance;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Lens It'),
-      ),
-      body: Container(
-        child: Container(
-          width: double.infinity,
-          height: 400,
-          color: Colors.black,
-          child: Image.file(img),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Image Labeling Firebase'),
+        ),
+        body: _buildBody(_file),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () async {
+            try {
+              // var file =
+              //     await ImagePicker.pickImage(source: ImageSource.gallery);
+              setState(() {
+                _file = widget.img;
+              });
+
+              var currentLabels =
+                  await detector.detectFromBinary(_file?.readAsBytesSync());
+              setState(() {
+                _currentLabels = currentLabels;
+              });
+            } catch (e) {
+              print(e.toString());
+            }
+          },
+          child: new Icon(Icons.select_all),
         ),
       ),
-    ));
+    );
+  }
+
+  //Build body
+  Widget _buildBody(File _file) {
+    return new Container(
+      child: new Column(
+        children: <Widget>[displaySelectedFile(_file), _buildList(_currentLabels)],
+      ),
+    );
+  }
+
+  Widget _buildList(List<VisionLabel> labels) {
+    if (labels == null || labels.length == 0 ) {
+      return new Text('Empty', textAlign: TextAlign.center);
+    }
+    return new Expanded(
+      child: new Container(    
+        child: new ListView.builder(
+            padding: const EdgeInsets.all(1.0),
+            itemCount: labels.length,
+            itemBuilder: (context, i) {
+              return _buildRow(labels[i].label, labels[i].confidence.toString(), labels[i].entityID);
+            }),
+      ),
+    );
+  }
+
+   Widget displaySelectedFile(File file) {
+    return new SizedBox(
+      // height: 200.0,
+      width: 150.0,
+      child: file == null
+          ? new Text('Sorry nothing selected!!')
+          : new Image.file(file),
+    );
+}
+
+  //Display labels
+  Widget _buildRow(String label, String confidence, String entityID) {
+    return new ListTile(
+      title: new Text(
+        "\nLabel: $label \nConfidence: $confidence \nEntityID: $entityID",
+      ),
+      dense: true,
+    );
   }
 }
